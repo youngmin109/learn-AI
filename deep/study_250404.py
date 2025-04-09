feb38ee3547e686c18125775c2ba0597aa940003
youngmin109 <qhqo3325@gmail.com>
2025-04-09 14:01:19 +0900

    study

--- deep/study_250404.py
+++ deep/study_250404.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error # MSE
 # 난수 생성
 # supppress=True : 아주 작은 숫자를 0으로 보이게함
 # precision=1 : 소수점 1자리까지만 출력
-# X  : 0 ~ 10사이 난수 3개 (3행 1열)
+# X  : 0 ~ 10사이 난수 100개 (100행 1열)
 np.set_printoptions(suppress=True, precision=1)
 X = np.random.rand(3, 1) * 10 
 
@@ -15,21 +15,17 @@ X = np.random.rand(3, 1) * 10
 # randn 정규분포 노이즈 (주로 -3~3, 여기에 2를 곱해서 스케일 업)
 y = 2.5 * X + np.random.randn(3, 1) * 2
 y = y.ravel() # 다차원 배열을 1차원으로 평탄화
-
-# 다차원 난수 생성 
-bar = np.random.rand(2, 3, 2, 1)
-print(bar)
-print(bar.ravel()) # 4차원 배열을 1차원으로 평탄화
-
+# SGD는 1차원 타겟값을 요구구s
 
 # 모델 생성, 학습, 예측, 평가
 model = SGDRegressor(
-    max_iter=100,
-    learning_rate="constant",
-    eta0=0.001,
-    penalty=None,
-    random_state=0
+    max_iter=100, # 학습 반복 횟수 
+    learning_rate="adaptive", # 학습률 자동조절
+    eta0=0.001, # 학습률 값
+    penalty=None, # 정규화 제거
+    random_state=0 # 결과 재현을 위한 시드 고정정
 )
 model.fit(X, y)
-y_pred = model.predict(X)
+y_pred = model.predict(X) # 예측 값 출력 (일반화)
 mse = mean_squared_error(y, y_pred)
+print(f"평균 제곱 오차(MSE): {mse:.4f}")
