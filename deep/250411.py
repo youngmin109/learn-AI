ab022cf1077cf6a0c5412058880df40da365d06d
youngmin109 <qhqo3325@gmail.com>
2025-04-11 17:28:01 +0900

    250411

---
+++ deep/250411.py
@@ -0,0 +1,25 @@
+
+samples = []
+y = []  # 정답
+
+w = 0.2
+b = 0.1
+
+gradient_w = 0.0
+gradient_b = 0.0
+
+for dp in zip(samples, y):
+    # 예측값
+    predict_y = w * f + b
+    
+    # Error : 예측 값 - 정답
+    error = predict_y - y
+    
+    # w의 기울기 : sum(Error * each f)/ 샘플의 개수
+    gradient_w += error * f
+    
+    # b의 기울기 : sum(Error) / 샘플의 개수
+    gradient_b += error
+    
+w = w - gradient_w / len(samples)
+b = b - gradient_b / len(samples)
