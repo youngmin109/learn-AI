eb99fe123812205e39906a86ad726fe1cbe0c5ad
youngmin109 <qhqo3325@gmail.com>
2025-04-30 23:12:43 +0900

    after tuning

--- deep/250411.py
+++ deep/250411.py
@@ -1,6 +1,5 @@
-import numpy as np
 
-samples = []
+samples = [3, 1, 2, 3]
 y = []  # 정답
 
 w = [0.2, 0.3]
@@ -9,15 +8,15 @@ b = 0.1
 gradient_w = [0.0, 0.0]
 gradient_b = 0.0
 
-for dp in zip(samples, y):
+for dp, y_ in zip(samples, y):
     # 예측값
-    predict_y = w * f + b
+    predict_y = w[0] * dp[0] + w[1] * dp[1] + b
     
     # Error : 예측 값 - 정답
-    error = predict_y - y
+    error = predict_y - y_
     
     # w의 기울기 : sum(Error * each f)/ 샘플의 개수
-    gradient_w += error * f
+    gradient_w += error * dp
     
     # b의 기울기 : sum(Error) / 샘플의 개수
     gradient_b += error
